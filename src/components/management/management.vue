fcb197f8be6324cc8210be1892fe4fe6a3c96dde
ljsy66 <2724238650@qq.com>
2019-03-24 21:47:29 +0800

    no message

--- src/components/management/management.vue
+++ src/components/management/management.vue
@@ -44,13 +44,10 @@
       </div>
     </card>
         <div class="col-lg-4">
-            <h4 class="title">Datetime Picker</h4>
+            <h4 class="title">Date Picker</h4>
             <fg-input>
-              <el-date-picker
-                v-model="dateTimePicker"
-                type="datetimerange"
-                placeholder="Datetime picker here"
-                :picker-options="pickerOptions1">
+              <el-date-picker v-model="datePicker" type="date" placeholder="Date picker here"
+                              :picker-options="pickerOptions1">
               </el-date-picker>
             </fg-input>
           </div>
@@ -136,19 +133,93 @@
   </div>
 </template>
 <script>
-  import {Table, TableColumn} from 'element-ui'
+  import {Table, TableColumn,DatePicker} from 'element-ui'
   import LSwitch from 'src/components/Switch.vue'
   export default{
     components: {
+        [DatePicker.name]: DatePicker,
       LSwitch,
       [Table.name]: Table,
       [TableColumn.name]: TableColumn
     },
-    data () {
+    data (){
       return {
+          switches: {
+          // defaultOn: true,
+          // defaultOff: false,
+          // plainOn: true,
+          // plainOff: false,
+          // withIconsOn: true,
+          // withIconsOff: false
+        },
+        sliders: {
+          // simple: 30,
+          // rangeSlider: [20, 50]
+        },
+        selects: {
+          // simple: '',
+          // countries: [{value: 'Bahasa Indonesia', label: 'Bahasa Indonesia'},
+          //   {value: 'Bahasa Melayu', label: 'Bahasa Melayu'},
+          //   {value: 'Català', label: 'Català'},
+          //   {value: 'Dansk', label: 'Dansk'},
+          //   {value: 'Deutsch', label: 'Deutsch'},
+          //   {value: 'English', label: 'English'},
+          //   {value: 'Español', label: 'Español'},
+          //   {value: 'Eλληνικά', label: 'Eλληνικά'},
+          //   {value: 'Français', label: 'Français'},
+          //   {value: 'Italiano', label: 'Italiano'},
+          //   {value: 'Magyar', label: 'Magyar'},
+          //   {value: 'Nederlands', label: 'Nederlands'},
+          //   {value: 'Norsk', label: 'Norsk'},
+          //   {value: 'Polski', label: 'Polski'},
+          //   {value: 'Português', label: 'Português'},
+          //   {value: 'Suomi', label: 'Suomi'},
+          //   {value: 'Svenska', label: 'Svenska'},
+          //   {value: 'Türkçe', label: 'Türkçe'},
+          //   {value: 'Íslenska', label: 'Íslenska'},
+          //   {value: 'Čeština', label: 'Čeština'},
+          //   {value: 'Русский', label: 'Русский'},
+          //   {value: 'ภาษาไทย', label: 'ภาษาไทย'},
+          //   {value: '中文 (简体)', label: '中文 (简体)'},
+          //   {value: 'W">中文 (繁體)', label: 'W">中文 (繁體)'},
+          //   {value: '日本語', label: '日本語'},
+          //   {value: '한국어', label: '한국어'}],
+          // multiple: 'ARS'
+        },
+        tags: {
+          // dynamicTags: ['Tag 1', 'Tag 2', 'Tag 3'],
+          // inputVisible: false,
+          // inputValue: ''
+        },
+
+        // pickerOptions1: {
+        //   shortcuts: [{
+        //     text: 'Today',
+        //     onClick (picker) {
+        //       picker.$emit('pick', new Date())
+        //     }
+        //   },
+        //   {
+        //     text: 'Yesterday',
+        //     onClick (picker) {
+        //       const date = new Date()
+        //       date.setTime(date.getTime() - 3600 * 1000 * 24)
+        //       picker.$emit('pick', date)
+        //     }
+        //   },
+        //   {
+        //     text: 'A week ago',
+        //     onClick (picker) {
+        //       const date = new Date()
+        //       date.setTime(date.getTime() - 3600 * 1000 * 24 * 7)
+        //       picker.$emit('pick', date)
+        //     }
+        //   }]
+        // },
+        datePicker: '',
          dateTimePicker: '',
          pickerOptions1: {
-          shortcuts: [{
+          shortcuts:[{
             text: 'Today',
             onClick (picker) {
               picker.$emit('pick', new Date())
@@ -238,6 +309,14 @@
       }
     },
     methods: {
+      handleInputConfirm () {
+        let inputValue = this.tags.inputValue
+        if (inputValue) {
+          this.tags.dynamicTags.push(inputValue)
+        }
+        this.tags.inputVisible = false
+        this.tags.inputValue = ''
+      },
       handleLike (index, row) {
         alert(`Your clicked on Like button`)
       },
